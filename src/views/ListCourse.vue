288f1dbc9cb5ec1ed83ba3123914d13ccbcb1bc8
nadia_lovely_29rpl <80080969+nadialvy@users.noreply.github.com>
2023-03-04 11:05:10 +0700

    fix url

--- src/views/ListCourse.vue
+++ src/views/ListCourse.vue
@@ -55,7 +55,7 @@ export default {
   },
   created() {
     const path = this.$route.params.course_category;
-    const url = `/src/data/${path}.json`;
+    const url = `/data/${path}.json`;
 
     axios
       .get(url)
